907eea4b4382f1e89c98a3e592cc1a0864546387
Gloriana Gonzalez <glogonzalez@Glorianas-MBP.lan>
2022-12-08 11:31:37 -0600

    created all thought routes

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,32 @@
+const router = require("express").Router();
+const {
+	getAllThoughts,
+	getThoughtById,
+	createThought,
+	updateThought,
+	deleteThought,
+	addReaction,
+	removeReaction,
+} = require("../../controllers/thought-controller");
+
+router.route("/")
+	.get(getAllThoughts);
+
+router.route("/:thoughtId")
+	.get(getThoughtById)
+	.put(updateThought);
+
+router.route("/:userId")
+	.post(createThought);
+
+router.route("/:userId/:thoughtId")
+	.delete(deleteThought);
+
+router.route("/:thoughtId/reactions")
+	.post(addReaction)
+
+router.route("/:thoughtId/reactions/:reactionId")
+	.delete(removeReaction);
+
+
+module.exports = router;
