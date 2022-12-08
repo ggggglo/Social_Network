eabf1bc81468374f4da1a38a53329a29ce7af60c
Gloriana Gonzalez <glogonzalez@Glorianas-MBP.lan>
2022-12-08 11:26:35 -0600

    controller to add and to remove a friend

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -86,4 +86,31 @@ const userController = {
 			.catch((err) => res.status(400).json(err));
 	},
 	// add a friend
+	addFriend({ params }, res) {
+		User.findOneAndUpdate(
+			{ _id: params.userId },
+			{ $addToSet: { friends: params.friendId } },
+			{ new: true, runValidators: true }
+		)
+			.then((dbUserData) => {
+				if (!dbUserData) {
+					res.status(404).json({ message: "No user found with this ID!" });
+					return;
+				}
+				res.json(dbUserData);
+			})
+			.catch((err) => res.json(err));
+	},
+	// remove a friend
+	removeFriend({ params }, res) {
+		User.findOneAndUpdate(
+			{ _id: params.userId },
+			{ $pull: { friends: params.friendId } },
+			{ new: true }
+		)
+			.then((dbUserData) => res.json(dbUserData))
+			.catch((err) => res.json(err));
+	},
+};
 
+module.exports = userController;
