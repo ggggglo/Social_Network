4ef1d477947fd848d84566485ed89836079129cc
Gloriana Gonzalez <glogonzalez@Glorianas-MBP.lan>
2022-12-08 11:28:07 -0600

    controller to create a new thought and to update a specific thought

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -28,3 +28,38 @@ const thoughtController = {
 				res.status(400).json(err);
 			});
 	},
+	// create a thought
+	createThought({ params, body }, res) {
+		Thought.create(body)
+			.then(({ _id }) => {
+				return User.findOneAndUpdate(
+					{ _id: params.userId },
+					{ $push: { thoughts: _id } },
+					{ new: true }
+				);
+			})
+			.then((dbUserData) => {
+				if (!dbUserData) {
+					res.status(404).json({ message: "No user found with this ID!" });
+					return;
+				}
+				res.json(dbUserData);
+			})
+			.catch((err) => res.json(err));
+	},
+	// update specific thought
+	updateThought({ params, body }, res) {
+		Thought.findOneAndUpdate({ _id: params.thoughtId }, body, {
+			new: true,
+			runValidators: true,
+		})
+			.then((dbThoughtData) => {
+				if (!dbThoughtData) {
+					res.status(404).json({ message: "No thought found with this ID!" });
+					return;
+				}
+				res.json(dbThoughtData);
+			})
+			.catch((err) => res.status(400).json(err));
+	},
+	
