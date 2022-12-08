562277e3565373a7ecd0d1952dba9204244ee437
Gloriana Gonzalez <glogonzalez@Glorianas-MBP.lan>
2022-12-08 11:27:32 -0600

    controller to get only one thought

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -12,3 +12,19 @@ const thoughtController = {
 				res.status(400).json(err);
 			});
 	},
+	// get specific thought
+	getThoughtById({ params }, res) {
+		Thought.findOne({ _id: params.thoughtId })
+			.select("-__v")
+			.then((dbThoughtData) => {
+				if (!dbThoughtData) {
+					res.status(404).json({ message: "No thought found with this ID!" });
+					return;
+				}
+				res.json(dbThoughtData);
+			})
+			.catch((err) => {
+				console.log(err);
+				res.status(400).json(err);
+			});
+	},
