3b67c9e5dc5f08c4fb72a4e39bf6355f9261babd
Gloriana Gonzalez <glogonzalez@Glorianas-MBP.lan>
2022-12-08 11:26:12 -0600

    controller to delete a user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -62,4 +62,28 @@ const userController = {
 			})
 			.catch((err) => res.status(400).json(err));
 	},
-	
+	// delete a user
+	deleteUser({ params }, res) {
+		User.findOneAndDelete({ _id: params.id })
+			.then((deletedUser) => {
+				if (!deletedUser) {
+					res.status(404).json({ message: "No user found with this ID!" });
+					return;
+				}
+				User.updateMany(
+					{ _id: { $in: deletedUser.friends } },
+					{ $pull: { friends: params.id } }
+				)
+					.then(() => {
+						Thought.deleteMany({ username: deletedUser.username })
+							.then(() => {
+								res.json({ message: "User deleted" });
+							})
+							.catch((err) => res.status(400).json(err));
+					})
+					.catch((err) => res.status(400).json(err));
+			})
+			.catch((err) => res.status(400).json(err));
+	},
+	// add a friend
+
